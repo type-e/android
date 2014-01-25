41c0b1d8caa332b1e8566d55af3c4423482d0ddb
Winson Lim <winsonlim@gmail.com>
2014-01-25 23:52:44 +0800

    added network layer classes. refactored ui components

---
+++ Type-E/src/main/java/com/typee/typee/network/registration/RegistrationParseService.java
@@ -0,0 +1,43 @@
+package com.typee.typee.network.registration;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.typee.typee.network.BaseParseService;
+
+/**
+ * Created by winsonlim on 20/1/14.
+ */
+public class RegistrationParseService extends BaseParseService {
+    private static RegistrationParseService instance;
+
+
+    public static RegistrationParseService getParseService() {
+        if (instance == null)
+            instance = new RegistrationParseService();
+
+        return instance;
+    }
+
+    public static void signUp(String username, String password, String emailAddress, int mobileNumber, final RegistrationCallback registerCallback) {
+        // todo to decide on uuid of registered users (mobile or email)
+
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(emailAddress);
+
+        // normal ParseObject
+        user.put("MobileNumber", mobileNumber);
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    registerCallback.registerSuccessful();
+                } else {
+                    registerCallback.registerUnsuccessful();
+                }
+            }
+        });
+    }
+}
