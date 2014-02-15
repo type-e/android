50f9c9b19a5d70495bc6457ce505788a4d195ef4
winsonlim <winsonlim@gmail.com>
2014-02-15 10:30:16 +0800

    Added config file

---
+++ Type-E/src/main/java/com/typee/typee/config/config.java
@@ -0,0 +1,14 @@
+package com.typee.typee.config;
+
+/**
+ * Created by winsonlim on 15/2/14.
+ */
+public class Config {
+
+	public static final String APP_NAME = "Type-E";
+
+	public static final String SMS_API_USERNAME = "APIN7VBTHSVFJ";
+	public static final String SMS_API_PASSWORD = "APIN7VBTHSVFJN7VBT";
+
+	public static final String SMS_API_URL = "http://gateway.onewaysms.sg:10002/api.aspx";
+}
