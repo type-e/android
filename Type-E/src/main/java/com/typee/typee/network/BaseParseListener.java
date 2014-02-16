303d7d021eb50beaae9ad38f5e2985f402f677ae
winsonlim <winsonlim@gmail.com>
2014-02-16 13:25:00 +0800

    fixed BaseParseListener

--- Type-E/src/main/java/com/typee/typee/network/BaseParseListener.java
+++ Type-E/src/main/java/com/typee/typee/network/BaseParseListener.java
@@ -2,14 +2,7 @@ package com.typee.typee.network;
 
 public interface BaseParseListener {
 
-	public void successful()
-	{
-		return true;
-	}
+    public void successful();
 
-
-	public void unsuccessful()
-	{
-		return false;
-	}
+    public void unsuccessful();
 }
