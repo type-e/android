62def19184a510a20e85d932ab2077d3a18d1bd6
winsonlim <winsonlim@gmail.com>
2014-02-19 12:02:38 +0800

    added string plural example

--- Type-E/src/main/java/com/typee/typee/ui/event/EventDetailsFragment.java
+++ Type-E/src/main/java/com/typee/typee/ui/event/EventDetailsFragment.java
@@ -14,46 +14,51 @@ import java.util.ArrayList;
 
 public class EventDetailsFragment extends BaseFragment {
 
-    private View rootView;
-    private QuiltView quiltView;
+	private View rootView;
+	private QuiltView quiltView;
 
-    public EventDetailsFragment() {
-        // Required empty public constructor
-    }
+	public EventDetailsFragment() {
+		// Required empty public constructor
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        rootView = inflater.inflate(R.layout.fragment_event_details, container, false);
-        quiltView = (QuiltView) rootView.findViewById(R.id.event_attendee_quilt);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		rootView = inflater.inflate(R.layout.fragment_event_details, container, false);
+		quiltView = (QuiltView) rootView.findViewById(R.id.event_attendee_quilt);
 
-        return rootView;
-    }
+		return rootView;
+	}
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        quiltView.setChildPadding(5);
-        quiltView.setOrientation(false);
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		quiltView.setChildPadding(5);
+		quiltView.setOrientation(false);
 
-        addTestQuilts(200);
-    }
+		addTestQuilts(200);
 
-    public void addTestQuilts(int num) {
-        ArrayList<ImageView> images = new ArrayList<ImageView>();
-        for (int i = 0; i < num; i++) {
-            ImageView image = new ImageView(getActivity());
-            image.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            if (i % 3 == 0)
-                image.setImageResource(R.drawable.winson);
-            else if (i % 3 == 1)
-                image.setImageResource(R.drawable.gx);
-            else
-                image.setImageResource(R.drawable.candy);
 
-            images.add(image);
-        }
-        quiltView.addPatchImages(images);
-    }
+		// TODO: Here is how you set plurals in code
+//	    countdownUnitTextView.setText(getResources().getQuantityString(R.plurals.event_timeout_units_minutes, minutes));
+
+	}
+
+	public void addTestQuilts(int num) {
+		ArrayList<ImageView> images = new ArrayList<ImageView>();
+		for (int i = 0; i < num; i++) {
+			ImageView image = new ImageView(getActivity());
+			image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			if (i % 3 == 0)
+				image.setImageResource(R.drawable.winson);
+			else if (i % 3 == 1)
+				image.setImageResource(R.drawable.gx);
+			else
+				image.setImageResource(R.drawable.candy);
+
+			images.add(image);
+		}
+		quiltView.addPatchImages(images);
+	}
 }
